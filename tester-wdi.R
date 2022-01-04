589a455c8edb9d2daa7b51f77acd71d2b66c2119
Lucas Stefano <lucassxs.sp@gmail.com>
2022-01-04 08:27:18 -0300

    arguments details

--- tester-wdi.R
+++ tester-wdi.R
@@ -8,12 +8,12 @@ library(WDI)        # for World Bank goodness
 library(kableExtra) # for tables, where appropriate
 
 WDI(
-  country = "BR", 
-  indicator = "NY.GDP.PCAP.KD",
-  start = 2001,
-  end = 2020,
-  extra = FALSE,
-  cache = NULL,
-  latest = NULL,
-  language = "en"
+  country = "BR", # vector of countries
+  indicator = "NY.GDP.PCAP.KD", # character vector of indicators code
+  start = 2001, # start date, usually a year in integer format
+  end = 2020, # end date, usually a year in integer format
+  extra = FALSE, # TRUE returns extra variables such as region, iso3c code, and incomeLevel.
+  cache = NULL, # NULL (optional) a list created by WDIcache() to be used with the extra=TRUE argument
+  latest = NULL, # Integer indicating the number of most recent non-NA values to get. Default is NULL. If specified, it overrides the start and end dates
+  language = "en" # ISO-2 code in lower case indicating in which language the characters should be provided. List of languages available with ‘WDI::languages_supported()‘. Default is English.
 )
