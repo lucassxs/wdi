4410b5a7663a1e06e6003e8e7c89b8508cd13118
Lucas Stefano <lucassxs.sp@gmail.com>
2022-01-04 08:35:32 -0300

    add plot example

--- tester-wdi.R
+++ tester-wdi.R
@@ -17,3 +17,31 @@ WDI(
   latest = NULL, # Integer indicating the number of most recent non-NA values to get. Default is NULL. If specified, it overrides the start and end dates
   language = "en" # ISO-2 code in lower case indicating in which language the characters should be provided. List of languages available with ‘WDI::languages_supported()‘. Default is English.
 )
+
+WDI(indicator = c("EG.ELC.ACCS.ZS", # access to electricity
+                  "BN.CAB.XOKA.GD.ZS", # current account balance
+                  "IC.BUS.DFRN.XQ", # ease of doing business
+                  "FP.CPI.TOTL.ZG", # CPI
+                  "FR.INR.LNDP"), # interest rate spread
+    start = 1960, end = 2020) %>% as_tibble() -> Data
+
+Data %>%
+  rename(elecperpop = 4,
+         cab = 5,
+         edb = 6,
+         cpi = 7,
+         ratespread = 8) -> Data
+
+Data %>%
+  filter(country == "Mexico") %>%
+  mutate(cpiprop = cpi/100) %>% # going somewhere with this...
+  ggplot(.,aes(year, cpiprop)) + 
+  theme_steve_web() + post_bg() +
+  geom_bar(stat="identity", alpha=.8, fill="#619cff", color="black") +
+  scale_x_continuous(breaks = seq(1960, 2020, by = 10)) +
+  # Below is why I like proportions
+  scale_y_continuous(labels = scales::percent) +
+  labs(x = "", y = "Consumer Price Index (Annual %)",
+       caption = "Data: International Monetary Fund, via {WDI}",
+       title = "The Consumer Price Index (Annual %) in Mexico, 1960-2020",
+       subtitle = "Debt crises and currency devaluations will account for the spikes you see.")
