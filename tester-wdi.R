aada663af4a6287b55f074d03be5dfec4842f3a7
Lucas Stefano <lucassxs.sp@gmail.com>
2022-01-04 08:23:42 -0300

    add country

---
+++ tester-wdi.R
@@ -0,0 +1,19 @@
+install.packages("WDI")
+install.packages('stevemisc')
+install.packages('kableExtra')
+
+library(tidyverse)  # for most things
+library(stevemisc)  # for my own vanity
+library(WDI)        # for World Bank goodness
+library(kableExtra) # for tables, where appropriate
+
+WDI(
+  country = "BR",
+  indicator = "NY.GDP.PCAP.KD",
+  start = 2001,
+  end = 2020,
+  extra = FALSE,
+  cache = NULL,
+  latest = NULL,
+  language = "en"
+)
